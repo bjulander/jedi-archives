f52d59e7473f544e8c1d72ff520f7f8bff6b8ca5
Blake Julander <13lakejulander@gmail.com>
2021-02-01 16:13:39 -0700

    Found the right api for this project as a hard code. Will change later now that i know it works

--- lib/api.rb
+++ lib/api.rb
@@ -1,4 +1,12 @@
-class API 
+class Api 
+
+    def self.get_info
+        url = "https://swapi.dev/api/planets/?search=kamino"
+        # url = "https://www.swapi.tech/api/planets/1/?format=wookiee"
+        response = HTTParty.get(url)
+        binding.pry
+    end
+
 
 
 
--- lib/cli.rb
+++ lib/cli.rb
@@ -2,6 +2,14 @@ class Cli
 
     def welcome
         puts "Thank you for using the NBA Databank! What are you looking for today?"
+        #Welcome master jedi. What are you looking for today? 
+        search_criteria
+    end
+
+    def search_criteria
+        puts "Please enter search criteria"
+        # input = gets.strip
+        Api.get_info
     end
 
     
