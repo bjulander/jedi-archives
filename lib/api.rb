4962bd10db28e080a493d5a13152ca88d48a2720
Blake Julander <13lakejulander@gmail.com>
2021-02-03 13:57:10 -0700

    Figured out how to send an instance through each category, exit, and return to the initial search. Will be implimenting for other categories.

--- lib/api.rb
+++ lib/api.rb
@@ -6,7 +6,7 @@ class Api
         # binding.pry
         response = HTTParty.get(url)
         if url.include?("people")
-            people_hash = {name: response["results"][0]["name"], birth_year: response["results"][0]["birth_year"], height: response["results"][0]["height"], homeworld: response["results"][0]["homeworld"]}
+            people_hash = {name: response["results"][0]["name"], birth_year: response["results"][0]["birth_year"], height: response["results"][0]["height"], homeworld: response["results"][0]["homeworld"], eye_color: response["results"][0]["eye_color"]}
             People.new(people_hash)
         elsif url.include?("planets")
             planets_hash = {name: response["results"][0]["name"], terrain: response["results"][0]["terrain"], climate: response["results"][0]["climate"], diameter: response["results"][0]["diameter"]}
@@ -20,25 +20,8 @@ class Api
         elsif url.include?("vehicles")
             vehicle_hash = {name: response["results"][0]["name"], model: response["results"][0]["model"], speed: response["results"][0]["max_atmosphering_speed"], }
             Vehicle.new(vehicle_hash)
-        else
+        else !url.include?("vehicles", "people", "planets", "species", "starships")
             puts "nil"
         end
-
-# binding.pry
-        #  people_hash = {name: response["results"][0]["name"], birth_year: response["results"][0]["birth_year"], height: response["results"][0]["height"], homeworld: response["results"][0]["homeworld"]}
-        #  planets_hash = {name: response["results"][0]["name"], terrain: response["results"][0]["terrain"], climate: response["results"][0]["climate"], diameter: response["results"][0]["diameter"]}
-        #  species_hash = {name: response["results"][0]["name"], classification: response["results"][0]["classification"], average_height: response["results"][0]["average_height"], average_lifespan: response["results"][0]["average_lifespan"], language: response["results"][0]["language"]}
-        #  starship_hash = {name: response["results"][0]["name"], model: response["results"][0]["model"], manufacturer: response["results"][0]["manufacturer"], crew: response["results"][0]["crew"], passengers: response["results"][0]["passengers"]}
-        #  vehicle_hash = {name: response["results"][0]["name"], model: response["results"][0]["model"], speed: response["results"][0]["max_atmosphering_speed"], }
-        
-        
-        #  People.new(people_hash)
-
-
     end
-
-
-
-
-
 end 
--- lib/cli.rb
+++ lib/cli.rb
@@ -31,6 +31,8 @@ class Cli
         when "vehicles"
             puts "Please enter a vehicle to begin your search"
             self.search(input)
+        when "exit!"
+            abort("Have a great day Master Jedi.")
         else
            puts "INVALID ENTRY. Please enter one of the following:" 
         end
@@ -40,12 +42,105 @@ end
     def search(category)
         input = gets.strip
         new_instance = Api.get_info(input, category)
-        self.category_attributes(new_instance)
+        case category
+        when "people"
+            self.people_options(new_instance)
+        when "planets"
+            self.planet_options(new_instance)
+        when "species"
+            self.species_options(new_instance)
+        when "starships"
+            self.starship_options(new_instance)
+        else "vehicles"
+            self.vehicle_options(new_instance)
+        end
     end
 
-    def category_attributes(instance)
-        puts "Please select from the following options:"
-
+    def people_options(instance)
+            puts "Please choose from the following options for #{instance.name}:"
+            puts "1. Full Name"
+            puts "2. Birth Year"
+            puts "3. Eye Color"
+            puts "4. Height"
+            puts "5. Homeworld"
+            puts "6. Begin a new search."
+            self.people_selection(instance)
     end
+
+    def people_selection(instance)
+        input = " "
+        while input != "exit!"
+        input = gets.strip
+        if input == "1"
+            puts "#{instance.name}"
+            puts " "
+            self.people_options(instance)
+        elsif input == "2"
+            puts "#{instance.name} was born #{instance.birth_year} (Before the Battle of Yavin)"
+            puts " "
+            self.people_options(instance)
+        elsif input == "3"
+            puts "#{instance.name} has #{instance.eye_color} eyes."
+            puts " "
+            self.people_options(instance)
+        elsif input == "4"
+            puts "#{instance.name} is #{instance.height} centimeters tall."
+            puts " "
+            self.people_options(instance)
+        elsif input == "5" 
+            self.people_options(instance)
+        elsif input == "6"
+            self.search_criteria
+        elsif input == "exit!"
+            abort("Have a great day Master Jedi.")
+        else
+            puts  "INVALID ENTRY. Please enter a valid option."
+        end
+    end
+    end
+
+    def planet_options(instance)
+        puts "Please choose from the following options for #{instance.name}:"
+        puts "1. Planet Name"
+        puts "2. Diameter"
+        puts "3. Terrain"
+        puts "4. Climate"
+        puts "5. Begin a new search."
+        self.planet_selection(instance)
+    end
+
+    def planet_selection(instance)
+        input = " "
+        while input != "exit!"
+        input = gets.strip
+        if input == "1"
+            puts "#{instance.name}"
+            puts " "
+            self.planet_options(instance)
+        elsif input == "2"
+            puts "#{instance.name} is #{instance.diameter} kilometers wide."
+            puts " "
+            self.planet_options(instance)
+        elsif input == "3"
+            puts "#{instance.name} has #{instance.terrain} terrain."
+            puts " "
+            self.planet_options(instance)
+        elsif input == "4"
+            puts "#{instance.name} is #{instance.climate}."
+            puts " "
+            self.planet_options(instance)
+        elsif input == "5"
+            self.search_criteria
+        elsif input == "exit!"
+            abort("Have a great day Master Jedi.")
+        else
+            puts  "INVALID ENTRY. Please enter a valid option."
+        end
+    end
+    end
+
+    
+
+
 
 end
