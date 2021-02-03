874b1daf351bfb787fa914dcc0f6f205a0998ffc
Blake Julander <13lakejulander@gmail.com>
2021-02-03 14:44:58 -0700

    Finished each call method for each class instance.

--- lib/cli.rb
+++ lib/cli.rb
@@ -139,6 +139,136 @@ end
     end
     end
 
+    def species_options(instance)
+        puts "Please choose from the following options for #{instance.name}:"
+            puts "1. Name"
+            puts "2. Classification"
+            puts "3. Average Height"
+            puts "4. Average Lifespan"
+            puts "5. Language"
+            puts "6. Begin a new search."
+            self.species_selection(instance)
+    end
+
+    def species_selection(instance)
+        input = " "
+        while input != "exit!"
+        input = gets.strip
+        if input == "1"
+            puts "#{instance.name}"
+            puts " "
+            self.species_options(instance)
+        elsif input == "2"
+            puts "#{instance.name} are classified as #{instance.classification}"
+            puts " "
+            self.species_options(instance)
+        elsif input == "3"
+            puts "#{instance.name} are #{instance.average_height} centimeters tall."
+            puts " "
+            self.species_options(instance)
+        elsif input == "4"
+            puts "#{instance.name} live on average #{instance.average_lifespan} years."
+            puts " "
+            self.species_options(instance)
+        elsif input == "5"
+            puts "#{instance.name} speak #{instance.language}"
+            puts " "
+            self.species_options(instance)
+        elsif input == "6"
+            self.search_criteria
+        elsif input == "exit!"
+            abort("Have a great day Master Jedi.")
+        else
+            puts  "INVALID ENTRY. Please enter a valid option."
+        end
+    end
+    end
+
+    def vehicle_options(instance)
+        puts "Please choose from the following options for #{instance.name}:"
+            puts "1. Name"
+            puts "2. Model"
+            puts "3. Top Speed"
+            puts "4. Begin a new search."
+            self.vehicle_selection(instance)
+    end
+
+    def vehicle_selection(instance)
+        input = " "
+        while input != "exit!"
+        input = gets.strip
+        if input == "1"
+            puts "#{instance.name}"
+            puts " "
+            self.vehicle_options(instance)
+        elsif input == "2"
+            puts "#{instance.name} is a #{instance.model} model."
+            puts " "
+            self.vehicle_options(instance)
+        elsif input == "3"
+            puts "The #{instance.name} has a top speed of #{instance.speed} kilometers per hour."
+            puts " "
+            self.vehicle_options(instance)
+        elsif input == "4"
+            self.search_criteria
+        elsif input == "exit!"
+            abort("Have a great day Master Jedi.")
+        else
+            puts  "INVALID ENTRY. Please enter a valid option."
+        end
+    end
+    end
+
+    def starship_options(instance)
+        puts "Please choose from the following options for #{instance.name}:"
+            puts "1. Name"
+            puts "2. Manufacturer"
+            puts "3. Model"
+            puts "4. Crew Count"
+            puts "5. Passenger Capacity"
+            puts "6. Begin a new search."
+            self.starship_selection(instance)
+    end
+
+    def starship_selection(instance)
+        input = " "
+        while input != "exit!"
+        input = gets.strip
+        if input == "1"
+            puts "#{instance.name}"
+            puts " "
+            self.starship_options(instance)
+        elsif input == "2"
+            puts "#{instance.name}s are made by #{instance.manufacturer}"
+            puts " "
+            self.starship_options(instance)
+        elsif input == "3"
+            puts "#{instance.name} is a #{instance.model} model."
+            puts " "
+            self.starship_options(instance)
+        elsif input == "4"
+            puts "The #{instance.name} needs #{instance.crew} personnel to operate."
+            puts " "
+            self.starship_options(instance)
+        elsif input == "5"
+            puts "The #{instance.name} can carry a maximum of #{instance.passengers} passengers."
+            puts " "
+            self.starship_options(instance)
+        elsif input == "6"
+            self.search_criteria
+        elsif input == "exit!"
+            abort("Have a great day Master Jedi.")
+        else
+            puts  "INVALID ENTRY. Please enter a valid option."
+        end
+    end
+    end
+
+
+
+
+
+
     
 
 
